4d995b8d4e32c8afeedf6096b3255aac1486581d
Marvinm1907 <mmontene@uoguelph.ca>
2026-02-27 10:42:28 -0500

    Added code for ManageEvent

--- src/main/java/com/guelph/engg1420finalprojectjavafx/ManageEvent.java
+++ src/main/java/com/guelph/engg1420finalprojectjavafx/ManageEvent.java
@@ -1,4 +1,29 @@
 package com.guelph.engg1420finalprojectjavafx;
 
+import java.util.ArrayList;
+
 public class ManageEvent {
+    private ArrayList<Event> eventList;
+
+    public ManageEvent(ArrayList<Event> events) {
+        this.eventList = new ArrayList<Event>();
+    }
+
+    public ArrayList<Event> getEvents() {
+        return new ArrayList<>();
+    }
+
+    public void addEvents(Event event) {
+        this.eventList.add(event);
+    }
+
+    public Event searchEvent(String title) {
+        for (Event event : this.eventList) {
+            if (event.getTitle().equalsIgnoreCase(title)) {
+                return event;
+            }
+        }
+        return null;
+    }
+
 }
